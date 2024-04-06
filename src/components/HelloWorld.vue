a56b9ce6b159fb9977b21cf78af75e5520d4ada6
ArdhikaFacrizSusilo <ardhika.facriz@gmail.com>
2024-04-06 12:33:13 +0700

    tugas 1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h2 style="color: white;">ARDHIKA FACRIZ SUSILO</h2>
+    <h3 style="font-size: 15px;">
+      <p><i class="bi bi-envelope-at"> ardhika.facriz@gmail.com</i> | <i class="bi bi-telephone"> 0898-8888-8888</i></p>
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 700;
+  font-size: 2.1rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,73 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>PROFIL</template>
+
+    Perkenalkan saya adalah seorang Web Development, disini saya memiliki semangat juang yang tinggi dalam bekerja secara mandiri maupun team. Jika Anda mencari seseorang yang siap menghadapi tantangan dengan senyum di wajahnya, maka Anda telah menemukannya! wkwk : )
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>DATA PRIBADI</template>
+
+    NAMA  : ARDHIKA FACRIZ SUSILO 
+    <br>
+    Tempat,Tgl lahir : Pekanbaru,12-12-2002
+    <br>
+    Jenis Kelamin : Laki-Laki
+    <br>
+    Agama : ISLAM
+    <br>
+    Status  : Belum menikah
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>PENDIDIKAN</template>
+
+    saat ini saya kuliah di salah satu kampus swasta di Riau yaitu
+    <a href="https://uir.ac.id/" target="_blank" rel="noopener">Universitas Islam Riau</a>. Sebelumnya saya bersekolah di SMA Negri <a href="" target="" rel="noopener">SMA 5 TUALANG</a>.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>DATA KEMAMPUAN</template>
+
+    Bahasa Pemograman : HTML/CSS, JavaScript, C++
+    <br>
+    Database  : SQL Server and MySQL
+    <br>
+    Desain  : Adobe Photoshop
+    <br>
+    Bahasa Percakapan : Bahasa Indonesia (Aktif) dan Bahasa Inggris (Pasif)
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>SOSIAL MEDIA</template>
+
+    <i class="bi bi-instagram"> Instagram : ardhikafacriz</i>
+    <br>
+    <i class="bi bi-facebook"> Facebook  : Ardhika Facriz</i>
+  </WelcomeItem>
+</template>
